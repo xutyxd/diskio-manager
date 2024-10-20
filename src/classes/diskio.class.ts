1049dc8fa9ec70847c01e160635d3bf7b30583af
Xuty <xutyxd@gmail.com>
2024-10-21 00:54:52 +0200

    feat(repository): wip

--- src/classes/diskio.class.ts
+++ src/classes/diskio.class.ts
@@ -1,15 +1,35 @@
-import fs from 'node:fs/promises';
+import fs from 'node:fs';
 
 export class DiskIO {
+    private LOCKED_SIZE: number;
+    private FREE_SIZE: number;
+    private USED_SIZE: number;
+    private PATH: string;
     // private Information;
     
-    public get information() {
-        return structuredClone(this.Information);
-    };
+    // public get information() {
+        // return structuredClone(this.Information);
+    // };
 
     constructor(path: string, size: number) {
         console.log('DiskIO class');
-        // Get information about the disk
-        // this.Information = require('diskinfo')(path);
+        // Check if the path exists
+        if (!fs.existsSync(path)) {
+            throw new Error('The path does not exist');
+        }
+        // Check if the path is a valid directory
+        const isDirectory = fs.statSync(path).isDirectory();
+        if (!isDirectory) {
+            throw new Error('The path is not a directory');
+        }
+        // Get mount point
+        const mountPoint = fs.realpathSync(path);
+        // Get free and used space
+        const freeSpace = fs.statSync(mountPoint).blocks * fs.statSync(mountPoint).size;
+        const usedSpace = fs.statSync(mountPoint).blocks * fs.statSync(mountPoint).size - freeSpace;
+        this.FREE_SIZE = freeSpace;
+        this.USED_SIZE = usedSpace;
+        this.PATH = path;
+        this.LOCKED_SIZE = size;
     }
 }
